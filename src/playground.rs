aff8585c10cbe36e2d761cf54ecbf0e495835238
Walter Moreira <walter@waltermoreira.net>
2022-12-25 23:57:53 -0600

    Add a sample of dynamic select

--- src/playground.rs
+++ src/playground.rs
@@ -2,11 +2,12 @@
 mod test {
     use anyhow::Result;
     use crossbeam::{
-        channel::{unbounded, Sender},
+        channel::{unbounded, Select, Sender},
         select,
     };
     use rand::{thread_rng, Rng};
     use std::{
+        collections::HashSet,
         thread,
         time::{Duration, Instant},
     };
@@ -86,6 +87,47 @@ mod test {
 
     #[test]
     fn test_dynamic_select() -> Result<()> {
+        // Test using `Select` with a dynamic list of operations.
+        let mut sel = Select::new();
+        let (s1, r1) = unbounded();
+        let (s2, r2) = unbounded();
+        let index1 = sel.recv(&r1);
+        let index2 = sel.recv(&r2);
+        println!("Added operation for thread 1 with index: {index1}");
+        println!("Added operation for thread 2 with index: {index2}");
+        let mut indices = HashSet::from([index1, index2]);
+
+        let start = Instant::now();
+        thread::spawn(move || random_sleeper(1, s1));
+        thread::spawn(move || random_sleeper(2, s2));
+        loop {
+            let oper = sel.select();
+            println!("Selected operation: {}", oper.index());
+            if oper.index() == index1 {
+                match oper.recv(&r1) {
+                    Ok(x) => println!("From thread 1 got {x}"),
+                    Err(_) => {
+                        println!("Thread 1 got error, removing it");
+                        println!("Time elapsed from start: {}", start.elapsed().as_secs());
+                        sel.remove(index1);
+                        indices.remove(&index1);
+                    }
+                };
+            } else if oper.index() == index2 {
+                match oper.recv(&r2) {
+                    Ok(x) => println!("From thread 1 got {x}"),
+                    Err(_) => {
+                        println!("Thread 2 got error, removing it");
+                        println!("Time elapsed from start: {}", start.elapsed().as_secs());
+                        sel.remove(index2);
+                        indices.remove(&index2);
+                    }
+                };
+            }
+            if indices.is_empty() {
+                break;
+            }
+        }
         Ok(())
     }
 }
