608befae541383829d6a7b9d8f56a48d0ea07717
Walter Moreira <walter@waltermoreira.net>
2023-01-04 11:32:16 -0600

    Add taskdep to nix flake

--- flake.nix
+++ flake.nix
@@ -6,9 +6,10 @@
     flake-utils.url = "github:numtide/flake-utils";
     crane.url = "github:ipetkov/crane";
     shell-utils.url = "github:waltermoreira/shell-utils";
+    taskdep.url = "github:waltermoreira/taskdep";
   };
 
-  outputs = { self, nixpkgs, flake-utils, crane, shell-utils }:
+  outputs = { self, nixpkgs, flake-utils, crane, shell-utils, taskdep }:
 
     with flake-utils.lib; eachSystem [
       system.x86_64-linux
@@ -19,6 +20,7 @@
           pkgs = nixpkgs.legacyPackages.${system};
           craneLib = crane.lib.${system};
           shell = shell-utils.myShell.${system};
+          taskdep-bin = taskdep.packages.${system}.taskdep;
         in
         rec {
           packages.supers =
@@ -36,6 +38,7 @@
               rustc
               jq
               go-task
+              taskdep-bin
             ];
           };
         });
