d281ea689e50d6903648de14ce9062e621a81e62
Walter Moreira <walter@waltermoreira.net>
2023-01-11 22:29:46 -0600

    Add flake for static compilation

--- flake.nix
+++ flake.nix
@@ -11,47 +11,86 @@
       url = "github:brocode/fblog";
       flake = false;
     };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, crane, shell-utils, taskdep, fblog }:
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , crane
+    , shell-utils
+    , taskdep
+    , fblog
+    , rust-overlay
+    }:
 
-    with flake-utils.lib; eachSystem [
-      system.x86_64-linux
-      system.x86_64-darwin
-    ]
-      (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          craneLib = crane.lib.${system};
-          shell = shell-utils.myShell.${system};
-          taskdep-bin = taskdep.packages.${system}.taskdep;
-        in
-        rec {
-          packages.supers =
-            craneLib.buildPackage {
+    let
+      dynamic = with flake-utils.lib; eachSystem [
+        system.x86_64-linux
+        system.x86_64-darwin
+      ]
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+            craneLib = crane.lib.${system};
+            shell = shell-utils.myShell.${system};
+            taskdep-bin = taskdep.packages.${system}.taskdep;
+          in
+          rec {
+            packages.supers =
+              craneLib.buildPackage {
+                src = craneLib.cleanCargoSource ./.;
+                buildInputs = with pkgs; [
+                  libiconv
+                ];
+              };
+            packages.fblog =
+              craneLib.buildPackage {
+                src = fblog;
+                buildInputs = with pkgs; [
+                  libiconv
+                ];
+              };
+            packages.default = packages.supers;
+            devShells.default = shell {
+              packages = with pkgs; [
+                packages.supers
+                packages.fblog
+                cargo
+                rustc
+                jq
+                go-task
+                taskdep-bin
+              ];
+            };
+          });
+      static = flake-utils.lib.eachSystem [ "x86_64-linux" ]
+        (system:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ (import rust-overlay) ];
+            };
+            rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+              targets = [ "x86_64-unknown-linux-musl" ];
+            };
+            craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+          in
+          {
+            packages.static-supers = craneLib.buildPackage {
               src = craneLib.cleanCargoSource ./.;
-              buildInputs = with pkgs; [
-                libiconv
-              ];
+              CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+              CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+              buildInputs = [ pkgs.libiconv ];
             };
-          packages.fblog =
-            craneLib.buildPackage {
-              src = fblog;
-              buildInputs = with pkgs; [
-                libiconv
-              ];
-            };
-          packages.default = packages.supers;
-          devShells.default = shell {
-            packages = with pkgs; [
-              packages.supers
-              packages.fblog
-              cargo
-              rustc
-              jq
-              go-task
-              taskdep-bin
-            ];
-          };
-        });
+          });
+    in
+    nixpkgs.lib.recursiveUpdate dynamic static;
 }
